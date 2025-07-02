23f58783c74fb24c4ebee97eb963bf0724bc5d98
Camila Aguiar <by.cmlla0107@gmail.com>
2025-07-02 11:49:19 -0300

    retirando

--- server/server.js
+++ server/server.js
@@ -24,13 +24,12 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-
 const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    host: "177.66.203.139",
+    port: 3306,
+    user: "psydb",
+    password: "Teste12345!",
+    database: "myanimelist",
 });
 
 con.connect((err) => {
